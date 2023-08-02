3d14fd1d64e06154724a899506729e52a3f9742c
David Egas <degasf@est.ups.edu.ec>
2023-08-01 22:07:38 -0500

    Inicio proyecto uno

---
+++ ProyectoUno/src/main/java/com/micro/usuario/usuarioservice/UsuarioServiceApplication.java
@@ -0,0 +1,13 @@
+package com.micro.usuario.usuarioservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsuarioServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsuarioServiceApplication.class, args);
+	}
+
+}
---
+++ ProyectoUno/src/test/java/com/micro/usuario/usuarioservice/UsuarioServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.micro.usuario.usuarioservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsuarioServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
