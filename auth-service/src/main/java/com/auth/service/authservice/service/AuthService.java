953edcae87a60b390f076ce43dd5485dd81c841f
DavidEF1996 <degasf@est.ups.edu.ec>
2023-09-14 22:38:48 -0500

    Integracion de JWT segunda parte

---
+++ auth-service/src/main/java/com/auth/service/authservice/service/AuthService.java
@@ -0,0 +1,73 @@
+package com.auth.service.authservice.service;
+
+import com.auth.service.authservice.dto.AuthUserDto;
+import com.auth.service.authservice.dto.TokenDto;
+import com.auth.service.authservice.entity.AuthUser;
+import com.auth.service.authservice.repository.AuthServiceRepository;
+import com.auth.service.authservice.security.JwtProvider;
+import com.netflix.discovery.converters.Auto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class AuthService {
+
+
+    @Autowired
+    private AuthServiceRepository authServiceRepository;
+
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private JwtProvider jwtProvider;
+
+    public AuthUser save(AuthUserDto authUserDto){
+        Optional <AuthUser> authUser = authServiceRepository.findByUserName(authUserDto.getUserName());
+
+        if(authUser.isPresent()){
+            return null;
+        }
+
+        String password = passwordEncoder.encode(authUserDto.getPassword());
+
+        AuthUser auth =  AuthUser.builder()
+                .userName(authUserDto.getUserName())
+                .password(password)
+                .build();
+        return authServiceRepository.save(auth);
+    }
+
+
+    public TokenDto login ( AuthUserDto authUserDto ){
+        Optional <AuthUser> authUser = authServiceRepository.findByUserName(authUserDto.getUserName());
+        if(authUser==null){
+            return  null;
+        }
+
+        if(passwordEncoder.matches(authUserDto.getPassword(), authUser.get().getPassword())){
+            return new TokenDto(jwtProvider.createToken(authUser.get()));
+        }
+
+        return null;
+    }
+
+
+    public TokenDto validate (String token){
+        if(!jwtProvider.validateToken(token)){
+            return null;
+        }
+
+        String userName = jwtProvider.userNameFromToken(token);
+
+        if(!authServiceRepository.findByUserName(userName).isPresent()){
+            return null;
+        }
+
+        return new TokenDto(token);
+    }
+}
